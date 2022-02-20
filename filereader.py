561a082dad60c6784612a8b1fb38b81cd333a0f7
yakorch <92575094+yakorch@users.noreply.github.com>
2022-02-20 22:12:38 +0200

    Add files via upload

---
+++ filereader.py
@@ -0,0 +1,17 @@
+def reading_file(path_file):
+    dataset = []
+    with open(path_file, "r", encoding="utf-8") as file:
+        file.readline()
+        for line in file:
+            line = line.rstrip()
+            line = line.replace("Setosa", "0")
+            line= line.replace("Versicolor", "1")
+            line = line.replace("Virginica", "2")
+            line = line.split(",")
+            line[-1] = int(line[-1][1])
+            line = list(map(lambda x: float(x), line))
+            line[-1] = int(line[-1])
+            dataset.append(line)
+    return dataset
+
+print(reading_file("SecondTask/iris.csv"))
